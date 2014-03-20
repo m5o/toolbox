af938379949a540f633c0abb64c2081aa929a9c2
m5o <m5o@gmx.com>
2014-03-20 22:38:16 +0100

    Update controllers_helper.rb

--- tools/controllers_helper.rb
+++ tools/controllers_helper.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 module ControllersHelper
 
-  # proof if this can be optimized with current_page?
+  # TODO proof if this can be optimized with current_page?
   # http://api.rubyonrails.org/classes/ActionView/Helpers/UrlHelper.html#method-i-current_page-3F
   def controller?(*controller)
     controller.include?(params[:controller])
