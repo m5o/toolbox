d73ebc4467465c91d492a934d9d0a6c23109c9d3
m5o <m5o@gmx.com>
2015-02-04 10:50:45 +0100

    use rspec 3.x syntax

--- tools/controllers_helper_spec.rb
+++ tools/controllers_helper_spec.rb
@@ -5,30 +5,30 @@ describe ControllersHelper do
 
   describe "#controller?" do
     before do
-      helper.stub(:params).and_return({ controller: "contents" })
+      allow(helper).to receive(:params).and_return(controller: "contents")
     end
 
     it "returns true if contoller match" do
-      helper.controller?("contents").should be true
+      expect(helper.controller?("contents")).to be true
     end
 
     it "returns true if match any" do
-      helper.controller?("contents", "users").should be true
+      expect(helper.controller?("contents", "users")).to be true
     end
   end
 
 
   describe "#action?" do
     before do
-      helper.stub(:params).and_return({ action: "index" })
+      allow(helper).to receive(:params).and_return(action: "index")
     end
 
     it "returns true if action match" do
-      helper.action?("index").should be true
+      expect(helper.action?("index")).to be true
     end
 
     it "returns false if action didn't match" do
-      helper.action?("new").should be false
+      expect(helper.action?("new")).to be false
     end
   end
 
