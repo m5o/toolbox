b5c4524aa646ad2bcb48acdcfc1ef6599190cdcb
m5o <m5o@gmx.com>
2016-04-07 14:06:46 +0200

    Update controllers_helper
    * Update controllers_helper.md
    * Update controllers_helper.rb
    * Update controllers_helper_spec.rb

--- tools/controllers_helper.rb
+++ tools/controllers_helper.rb
@@ -1,6 +1,10 @@
 # encoding: UTF-8
 module ControllersHelper
 
+  def namespace?(*controller)
+    controller.include?(params[:controller].split('/').first)
+  end
+
   # TODO proof if this can be optimized with current_page?
   # http://api.rubyonrails.org/classes/ActionView/Helpers/UrlHelper.html#method-i-current_page-3F
   def controller?(*controller)
